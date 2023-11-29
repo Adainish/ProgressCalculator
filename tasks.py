9266976530e87f684b5d078e240b05fba42b5646
Adenydd Stuut <wingletdev@gmai.com>
2023-11-29 11:12:34 +0100

    FEAT:  - Added CLI support, with a fallback to input args if required options are forgotten  - Added support for customising the progress bar description  - Added support for customising the progress bar colour CHANGE:  - Rewrote the code to be more pythonic, as well as easily readable

--- tasks.py
+++ tasks.py
@@ -1,12 +1,33 @@
+from datetime import datetime
 from invoke import task
 
 import timeframe
 
 
-@task
-def calculate(ct):
+@task(optional=['start_date', 'end_date', 'total_hours', 'hours_per_day', 'progress_target', 'colour'])
+def calculate(ctx, start_date=None, end_date=None, total_hours=None, hours_per_day=None, progress_target=None, colour=None):
     timeframe_calc = timeframe.TimeCalculator()
-    timeframe_calc.get_user_input_dates()
-    timeframe_calc.get_user_input_hours()
+    if start_date is not None:
+        timeframe_calc.starting_date = datetime.strptime(start_date, "%Y-%m-%d")
+    else:
+        timeframe_calc.get_user_input_starting_date()
+    if end_date is not None:
+        timeframe_calc.ending_date = datetime.strptime(end_date, "%Y-%m-%d")
+    else:
+        timeframe_calc.get_user_input_end_date()
+    if total_hours is not None:
+        total_hours = int(total_hours)
+        timeframe_calc.required_hours = total_hours
+        timeframe_calc.hours_left = total_hours
+    else:
+        timeframe_calc.get_user_input_hours()
+    if hours_per_day is not None:
+        timeframe_calc.hours_per_day = int(hours_per_day)
+    else:
+        timeframe_calc.get_user_input_hours_per_day()
+    if progress_target is not None:
+        timeframe_calc.progress_target = progress_target
+    if colour is not None:
+        timeframe_calc.colour = colour.upper()
     timeframe_calc.display_progress()
 
--- timeframe.py
+++ timeframe.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import time
 from tqdm import tqdm
 
 
@@ -8,6 +9,9 @@ class TimeCalculator:
         self.ending_date = None
         self.required_hours = None
         self.hours_left = None
+        self.hours_per_day = None
+        self.progress_target = 'Progress'
+        self.colour = 'WHITE'
 
     def get_user_input_dates(self):
         start_date_str = input("Enter the starting date (YYYY-MM-DD): ")
@@ -15,27 +19,40 @@ class TimeCalculator:
         self.starting_date = datetime.strptime(start_date_str, "%Y-%m-%d")
         self.ending_date = datetime.strptime(end_date_str, "%Y-%m-%d")
 
+    def get_user_input_starting_date(self):
+        start_date_str = input("Enter the starting date (YYYY-MM-DD): ")
+        self.starting_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+
+    def get_user_input_end_date(self):
+        end_date_str = input("Enter the ending date (YYYY-MM-DD): ")
+        self.ending_date = datetime.strptime(end_date_str, "%Y-%m-%d")
+
     def get_user_input_hours(self):
         self.required_hours = int(input("Enter the required hours: "))
         self.hours_left = self.required_hours
 
-    def is_weekday(self, date: datetime) -> bool:
-        return date.weekday() < 5  # 0-4 corresponds to Monday to Friday
+    def get_user_input_hours_per_day(self):
+        self.hours_per_day = int(input("Enter the hours counted per day: "))
+
+    def date_range(self, start_date, end_date):
+        delta = end_date - start_date
+        for i in range(delta.days + 1):
+            current_date = start_date + timedelta(days=i)
+            if current_date.weekday() < 5:  # 0-4 corresponds to Monday to Friday
+                yield current_date
 
     def calculate_time_left(self) -> int:
-        current_date = self.starting_date
         total_hours = 0
-        while current_date <= self.ending_date:
+        for current_date in self.date_range(self.starting_date, self.ending_date):
             if current_date >= datetime.today():
                 break
-            if self.is_weekday(current_date):
-                total_hours += min(8, self.hours_left)
-                self.hours_left -= min(8, self.hours_left)
-            current_date += timedelta(days=1)
+            total_hours += min(self.hours_per_day, self.hours_left)
+            self.hours_left -= min(self.hours_per_day, self.hours_left)
         return total_hours
 
     def display_progress(self):
         total_hours = self.calculate_time_left()
-        with tqdm(total=self.required_hours, desc='Progress') as pbar:
+        with tqdm(total=self.required_hours, desc=self.progress_target, colour=self.colour) as pbar:
             for _ in range(total_hours):
+                time.sleep(0.001)
                 pbar.update()
